8f25e8acccbdafd38a332661652ad7a3615f2fb6
Saad <saad.qu01@gmail.com>
2022-01-30 22:37:02 +0500

    [feature]: add controller exception handling

--- controllers/students/add.js
+++ controllers/students/add.js
@@ -7,6 +7,10 @@ module.exports = async (req, res) => {
       return res.status(400).json({ errors: errors.array() });
     }
     const data = req.body;
-    const student = await Students.create(data);
-    return res.json({ msg: 'success', data: student });
+    try {
+        const student = await Students.create(data);
+        return res.json({ msg: 'success', data: student });
+    } catch (error) {
+        return res.status(400).json({ msg: 'error', data: err.message });
+    }
 }
