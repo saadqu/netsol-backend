85fb2d699ed1e9ceb98e38557d9432e3b904cb91
Saad <saad.qu01@gmail.com>
2022-01-30 22:15:49 +0500

    [feature]: add route added

--- routes/una/students.js
+++ routes/una/students.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const router = express.Router();
 router.get('/', require('../../controllers/students/getAll'));
+router.post(
+    '/',
+    body('f_name').not().isEmpty().trim().escape(),
+    body('l_name').not().isEmpty().trim().escape(),
+    body('age').isNumeric(),
+    body('course').not().isEmpty().trim().escape(),
+    require('../../controllers/students/add')
+);
 
 module.exports = router;
