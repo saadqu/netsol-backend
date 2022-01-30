93eed87bb3c2ffec945874ce21dd9801ee112e82
Saad <saad.qu01@gmail.com>
2022-01-30 23:03:06 +0500

    [feature]: getAll controller exception handling

--- controllers/students/getAll.js
+++ controllers/students/getAll.js
@@ -1,6 +1,10 @@
 const Students = require('../../models/students');
 
 module.exports = async (req, res) => {
-    const students = await Students.findAndCountAll();
-    return res.json({ msg: 'success', ...students });
+    try {
+        const students = await Students.findAndCountAll();
+        return res.json({ msg: 'success', ...students });
+    } catch (error) {
+        return res.status(400).json({ msg: 'error', data: err.message });
+    }
 }
