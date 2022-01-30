f6b4319250e0cfd8a779fe36c4dc4e6206aba6a8
Saad <saad.qu01@gmail.com>
2022-01-30 21:53:14 +0500

    [feature]: students getAll added

---
+++ controllers/students/getAll.js
@@ -0,0 +1,6 @@
+const Students = require('../../models/students');
+
+module.exports = async(req, res) => {
+    const students = await Students.findAll();
+    return res.json({ msg: 'success', data: students });
+}
