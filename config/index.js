fcf2942ca8b1af3adc90adc1bd5d5aba00aa1309
Saad <saad.qu01@gmail.com>
2022-02-04 21:34:48 +0500

    feat(db): added db info

--- config/index.js
+++ config/index.js
@@ -3,5 +3,11 @@ require('dotenv').config();
 module.exports = {
     server: {
         PORT: process.env.PORT || 3000,
+    },
+    database: {
+        HOST: process.env.DB_HOST,
+        USER: process.env.DB_USER,
+        PASSWORD: process.env.DB_PASSWORD,
+        DB: process.env.DB_NAME
     }
 };
